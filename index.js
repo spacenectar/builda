4fa2f4817a30dabef72126983c6e8058b89eec1e
Alex Ward <alex@volcaniccreations.com>
2017-04-03 14:51:45 +0100

    added __dirname

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ var createFile = function (fileName, componentName, doc) {
   // Tell the user what is happening
   console.log(chalk.blue('\nCreating', fileName, '...'))
   // Bring in the scaffold file (note this should point to the scaffold file in node modules)
-  var scaffold = './scaffold/' + fileName
+  var scaffold = __dirname + '/scaffold/' + fileName
   fs.readFile(scaffold, 'utf8', function (err, data) {
     if (err) return console.log(chalk.red(err))
     var result = data.replace(/%cname%/g, componentName)
