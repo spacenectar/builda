74913dbce3c4e27de2941e67e9d21bbe1bd50d68
Alex Foxleigh <alex@alexfoxleigh.com>
2020-02-19 11:39:01 +0000

    Restructures scaffold to provide terser syntax. TS is now actually used instead of just a file extension. Closes #2

--- scaffold/index.jsx
+++ scaffold/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-%styleimport%const %ComponentExample% = ({name, colour}) => {
-    <div className={`example style-${colour}`}>
-        This is an example component, the name provided to it was {name}
-    </div>
-}
+%styleimport%const %ComponentExample% = ({name, colour}) =>
+  <div className={`example style-${colour}`}>
+    This is an example component, the name provided to it was {name}
+  </div>
+
 
 export default %ComponentExample%
--- scaffold/index.tsx
+++ scaffold/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-%styleimport%const %ComponentExample% = ({name, colour}) => {
-    <div className={`example style--${colour}`}>
-        This is an example component, the name provided to it was {name}
-    </div>
+interface Props {
+  name: string
+  colour: string
 }
+
+%styleimport%const %ComponentExample%: React.FunctionComponent<Props> = ({ name, colour }: Props) =>
+  <div className={`example style--${colour}`}>
+    This is an example component, the name provided to it was {name}
+  </div>
+
 
 export default %ComponentExample%
