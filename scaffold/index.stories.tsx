c43946c109b2456154322cd881938bfadfee24e7
Alex Foxleigh <alex@alexfoxleigh.com>
2020-03-04 14:06:55 +0000

    adds tsx style storybook files

---
+++ scaffold/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// Import component files
+import %ComponentExample% from './index'
+import docs from './README.md'
+
+
+export default {
+  title: 'local/%ComponentExample%',
+  component: %ComponentExample%,
+  decorators: [],
+  parameters: {
+    readme: {
+      // Show readme before story
+      content: docs
+    }
+  }
+}
+
+export const Default: React.FunctionComponent = () => <%ComponentExample% />
+
