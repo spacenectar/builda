098dceed7123e8997c8a9d682707dacf0f02e0f0
Alex Foxleigh <alex@alexfoxleigh.com>
2022-04-16 23:51:46 +0100

    Updated change case to work a little better

--- scripts/change-case.js
+++ scripts/change-case.js
@@ -1,12 +1,16 @@
-module.exports =  changeCase = (str, type) => {
+module.exports =  changeCase = (input, type) => {
+  const str = input.replace(/[,_]/g, ' ').replace(/&/g, ' and ').replace(/@/g, ' at ').replace(/\s+/g, '-').trim();
+  let output = '';
   if (type === 'sentence') {
-    const string = str.replace(/([A-Z])/g, ' $1').toLowerCase()
-    return string.charAt(0).toUpperCase() + string.slice(1)
+    // Output the component name however they provided it
+    output = input
   } else if (type === 'kebab') {
-    return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+    output = str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
   } else if (type === 'pascal') {
-    return str.split('-').map(word => {
+    output = str.split('-').map(word => {
       return word.charAt(0).toUpperCase() + word.slice(1)
     }).join('')
   }
+
+  return output
 }
