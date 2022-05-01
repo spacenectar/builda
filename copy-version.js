1000051a7f9fef71bf9653993261ab2740d92c08
Alex Foxleigh <alex@alexfoxleigh.com>
2022-05-01 21:35:29 +0100

    copy-version extended to generate globals to add more functionality

--- copy-version.js
+++
@@ -1,7 +0,0 @@
-// As we need the package.json version to be available in the build, we need to copy it from the root of the project
-
-const fs = require('fs');
-
-const version = require('./package.json').version;
-
-fs.writeFileSync('./src/version.ts', `export default '${version}';`);
---
+++ generate-globals.js
@@ -0,0 +1,15 @@
+// As we need the package.json version to be available in the build, we need to copy it from the root of the project
+
+const fs = require('fs');
+
+const version = require('./package.json').version;
+
+const globals = {
+  version,
+  configFileName: '.builda.yml',
+  configFileNameLegacy: '.buildcomrc'
+};
+
+const output = `export default ${JSON.stringify(globals, null, 2)};`;
+
+fs.writeFileSync('./src/helpers/globals.ts', output);
