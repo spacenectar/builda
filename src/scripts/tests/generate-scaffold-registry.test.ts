0bf3d897c53d46ec8a57d90494ce8cfe6dfbbce8
Alex Foxleigh <alex@alexfoxleigh.com>
2022-07-18 20:43:17 +0100

    Updated github workflow and fixed typo causing test to fail

--- src/scripts/tests/generate-scaffold-registry.test.ts
+++ src/scripts/tests/generate-scaffold-registry.test.ts
@@ -1,18 +1,20 @@
 import fs from 'fs';
 
 import generateScaffoldRegistry from '@scripts/generate-scaffold-registry';
+import path from 'path';
+
+const scaffoldPath = path.resolve('./src/mocks/scaffolds');
+const registryPath = `${scaffoldPath}/test-scaffold/registry.json`;
+
+beforeAll(() => {
+  if (fs.existsSync(registryPath)) {
+    fs.rmSync(registryPath);
+  }
+});
 
 describe('generateScaffoldRegistry() function happy path', () => {
-  afterEach(() => {
-    jest.restoreAllMocks();
-    if (fs.existsSync('./src/mocks/scaffolds/test-scaffold/registry.json')) {
-      fs.rmSync('./src/mocks/scaffolds/test-scaffold/registry.json');
-    }
-  });
   test('generateScaffoldRegistry() function generates a scaffold registry', () => {
-    generateScaffoldRegistry('./src/mocks/scaffolds/test-scaffold');
-    expect(
-      fs.existsSync('./src/mocks/scaffolds/test-scaffold/registry.json')
-    ).toBe(true);
+    generateScaffoldRegistry(scaffoldPath);
+    expect(fs.existsSync(registryPath)).toBe(true);
   });
 });
