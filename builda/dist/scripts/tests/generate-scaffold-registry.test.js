7eb82116f9fa26f0b2ee51cfe4b7c486027fa0af
Alex Foxleigh <alex@alexfoxleigh.com>
2022-08-19 11:03:25 +0100

    Hopefully fixed the flaky test issue

--- builda/dist/scripts/tests/generate-scaffold-registry.test.js
+++ builda/dist/scripts/tests/generate-scaffold-registry.test.js
@@ -6,12 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const generate_scaffold_registry_1 = __importDefault(require("../generate-scaffold-registry"));
 const path_1 = __importDefault(require("path"));
+const init_1 = __importDefault(require("../init"));
+const preset_answers_1 = __importDefault(require("../../mocks/preset-answers"));
 const scaffoldPath = path_1.default.resolve('./src/mocks/scaffolds');
 const registryPath = `${scaffoldPath}/test-scaffold/registry.json`;
-beforeAll(() => {
+beforeAll(async () => {
     if (fs_1.default.existsSync(registryPath)) {
         fs_1.default.rmSync(registryPath);
     }
+    jest.spyOn(console, 'log').mockImplementation(() => null);
+    await (0, init_1.default)({ presetAnswers: preset_answers_1.default, force: true });
+});
+afterAll(() => {
+    jest.restoreAllMocks();
 });
 describe('generateScaffoldRegistry() function happy path', () => {
     test('generateScaffoldRegistry() function generates a scaffold registry', () => {
--- builda/src/scripts/tests/generate-scaffold-registry.test.ts
+++ builda/src/scripts/tests/generate-scaffold-registry.test.ts
@@ -2,14 +2,22 @@ import fs from 'fs';
 
 import generateScaffoldRegistry from '@scripts/generate-scaffold-registry';
 import path from 'path';
+import init from '@scripts/init';
+import presetAnswers from '@mocks/preset-answers';
 
 const scaffoldPath = path.resolve('./src/mocks/scaffolds');
 const registryPath = `${scaffoldPath}/test-scaffold/registry.json`;
 
-beforeAll(() => {
+beforeAll(async () => {
   if (fs.existsSync(registryPath)) {
     fs.rmSync(registryPath);
   }
+  jest.spyOn(console, 'log').mockImplementation(() => null);
+  await init({presetAnswers, force: true});
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
 });
 
 describe('generateScaffoldRegistry() function happy path', () => {
