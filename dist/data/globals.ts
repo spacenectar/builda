1d0e9ce1aa776781796a4ec2c2cb0c9bd5dd3d7d
Alex Foxleigh <alex@alexfoxleigh.com>
2022-09-21 12:06:30 +0100

    Updates a test (still failing thought) and runs a build

--- dist/data/globals.ts
+++ dist/data/globals.ts
@@ -1,1 +1,1 @@
-export default {"version":"5.0.0","buildaDir":".builda","configFileName":"config.json","websiteUrl":"https://www.builda.app","repository":{"type":"git","url":"git+https://github.com/spacenectar/builda"}};
+export default {"version":"5.0.0","buildaDir":".builda","configFileName":"config.json","websiteUrl":"https://www.builda.app","buildaReadmeFileName":"getting-started.md","repository":{"type":"git","url":"git+https://github.com/spacenectar/builda"}};
--- dist/helpers/write-file.js
+++ dist/helpers/write-file.js
@@ -8,18 +8,20 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const string_functions_1 = __importDefault(require("./string-functions"));
 const prettier_1 = __importDefault(require("prettier"));
-const writeFile = ({ file, output_dir, substitute, name }) => {
+const writeFile = ({ file, rename, output_dir, substitute, name }) => {
     const fileName = file.split('/').pop();
     // get the file contents
     const fileContents = fs_1.default.readFileSync(path_1.default.resolve(file), 'utf8');
     // replace the each placeholder with the correctly formatted name
-    let newContents = fileContents &&
+    let newContents = fileContents;
+    if (name) {
         fileContents
             .replace(/%KEBAB_CASE%/g, (0, string_functions_1.default)(name, 'kebabCase'))
             .replace(/%CAMEL_CASE%/g, (0, string_functions_1.default)(name, 'camelCase'))
             .replace(/%SNAKE_CASE%/g, (0, string_functions_1.default)(name, 'snakeCase'))
             .replace(/%PASCAL_CASE%/g, (0, string_functions_1.default)(name, 'pascalCase'))
             .replace(/%SENTENCE_CASE%/g, (0, string_functions_1.default)(name, 'sentenceCase'));
+    }
     // Replace custom substitutions
     if (substitute && substitute.length > 0) {
         substitute.forEach((sub) => {
@@ -33,9 +35,9 @@ const writeFile = ({ file, output_dir, substitute, name }) => {
     });
     // write the new file contents to the output directory
     if (newContents) {
-        return fs_1.default.writeFileSync(`${output_dir}/${fileName}`, newContents);
+        return fs_1.default.writeFileSync(`${output_dir}/${rename || fileName}`, newContents);
     }
-    throw new Error(`Could not write file ${fileName}`);
+    throw new Error(`Could not write file ${rename || fileName}`);
 };
 exports.writeFile = writeFile;
 exports.default = exports.writeFile;
