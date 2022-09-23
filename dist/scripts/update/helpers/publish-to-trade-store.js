0200448f923738183af7f071c5750a3e415737da
Alex Foxleigh <alex@alexfoxleigh.com>
2022-09-23 06:48:13 +0100

    fixup! Moved tests for new into folder and updated some typescript issues (deleted dist for now as it won't build properly until the refactoring is done and it's bugging me

--- dist/scripts/update/helpers/publish-to-trade-store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.publishToTradeStore = void 0;
-const publishToTradeStore = async () => {
-    // TODO: Publish to trade store
-    return true;
-};
-exports.publishToTradeStore = publishToTradeStore;
