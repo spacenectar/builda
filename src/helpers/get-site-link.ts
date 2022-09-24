ed6ad66efe9df678b1a7fef58a35148605a6d871
Alex Foxleigh <alex@alexfoxleigh.com>
2022-09-24 22:20:07 +0100

    Adds function to allow me to put all site documentation links in a single file

---
+++ src/helpers/get-site-link.ts
@@ -0,0 +1,24 @@
+import globals from 'data/globals';
+import pathFile from 'data/website-paths.json';
+
+type TPaths = {
+  [key: string]: {
+    [key: string]: string;
+  };
+};
+
+export default (slug: string, anchor?: string) => {
+  const { websiteUrl } = globals;
+  const pathParts = slug.split('/');
+  const paths = pathFile as TPaths;
+  const rootPath = pathParts[0];
+
+  const outputPaths = pathParts.map((pathPart) => {
+    if (pathPart === rootPath) {
+      return pathPart;
+    }
+    return paths[rootPath][pathPart];
+  });
+
+  return `${websiteUrl}/${outputPaths.join('/')}${anchor ? `#${anchor}` : ''}`;
+};
