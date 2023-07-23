9b9998ea4f80f872d0f424eec6fd599d2df35802
Alex Foxleigh <alex@alexfoxleigh.com>
2023-07-23 12:41:25 +0100

    Adds 'convert-to-builda-script' helper

--- src/helpers/string/index.ts
+++ src/helpers/string/index.ts
@@ -4,5 +4,6 @@ export { default as detectPathType } from './detect-path-type';
 export { default as normaliseCase } from './normalise-case';
 export { default as convertNumbersToWords } from './convert-numbers-to-words';
 export { default as convertSymbolsToWords } from './convert-symbols-to-words';
+export { default as convertToBuildaScript } from './convert-to-builda-script';
 export { default as changeCase } from './change-case';
 export { default as pluralise } from './pluralise';
